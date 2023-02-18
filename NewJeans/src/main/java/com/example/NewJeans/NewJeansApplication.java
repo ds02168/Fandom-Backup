6436c6d30bdd997988c23963299bc3efc4fb8e5b
ds02168 <1995musso@gmail.com>
2023-02-18 22:16:03 +0900

    백업용 레포지토리, 원본:https://github.com/itcenTeam6/Fandom.git

---
+++ NewJeans/src/main/java/com/example/NewJeans/NewJeansApplication.java
@@ -0,0 +1,15 @@
+package com.example.NewJeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class NewJeansApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJeansApplication.class, args);
+	}
+
+}
